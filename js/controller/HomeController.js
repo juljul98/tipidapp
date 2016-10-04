ac36d89fa998a26a0e00a07f7f70e16bb83a6ad0
ph2150091 <ph2150091@transcosmos-ph.local>
2016-10-04 17:50:50 +0800

    50% frontend

--- js/controller/HomeController.js
+++ js/controller/HomeController.js
@@ -3,5 +3,12 @@ tipidapp.controller('HomeController', function($scope) {
 	$scope.share = function() {
 		$('#modalsocial').openModal();
 	}
+	$scope.modalimage = function() {
+		$('#modalimages').openModal();
+	}
+	$scope.cancelupload = function() {
+		confirm('Do you want to cancel upload');
+	}
+
 
 });
--- js/controller/NavigationController.js
+++ js/controller/NavigationController.js
@@ -1,4 +1,4 @@
-tipidapp.controller('NavigationController', function($scope) {
+tipidapp.controller('NavigationController', function($scope, $rootScope, $window) {
 
 	$scope.navigation = ['home', 'tools', 'settings'];
 	$scope.activeMenu = $scope.navigation[0];
@@ -6,5 +6,24 @@ tipidapp.controller('NavigationController', function($scope) {
     $scope.setActive = function(navigation) {
     	$scope.activeMenu = navigation
 	}
-	
+
+	$scope.toggleaction = 'search';
+	$scope.toggleactive = false;
+	var action = false;
+	$scope.togglesearch = function() {
+		if ( action == false ) {
+			// Search mode
+			$scope.toggleaction = 'close';
+			$scope.toggleactive = true;
+			action = true;
+		} else {
+			// Default
+			$scope.toggleaction = 'search';
+			$scope.toggleactive = false;
+			action = false;
+		}
+	}
+	$rootScope.backbutton = function() {
+		$window.history.back();
+	}
 });
